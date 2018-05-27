871b7477f0d001d1f651f26ff1181b32a1509371
amaddatu <amaddatu@gmail.com>
2018-05-27 17:01:05 -0400

    Add new gulp configuration for an automated workflow

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+// in order to use this file
+// both gulp and gulp-sass and node-sass must be install globally
+// "npm install -g node-sass gulp gulp-cli"
+// type "gulp default" into the terminal
+// Sass configuration
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+    gulp.src('assets/scss/main.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('assets/css'))
+});
+
+gulp.task('default', ['sass'], function() {
+    gulp.watch('assets/scss/main.scss', ['sass']);
+})
