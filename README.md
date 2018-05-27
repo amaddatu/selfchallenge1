7bf759be3d9ce1456641ac862da145fbed6b1815
amaddatu <amaddatu@gmail.com>
2018-05-27 19:52:54 -0400

    Update commentary on the installation of this project

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # selfchallenge1
 A quick new project to challenge myself with responsive CSS
+
+# How to install
+navigation to where the project was installed
+
+type `npm install` into the console
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
+// This configuration is OPTIONAL
+
 // in order to use this file
 // both gulp and gulp-sass and node-sass must be install globally
 // "npm install -g node-sass gulp gulp-cli"
 // type "gulp default" into the terminal
+// CTRL + C / CMD + C to cancel
+
 // Sass configuration
 var gulp = require('gulp');
 var sass = require('gulp-sass');
