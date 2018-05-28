63afd5e9cbcc7be4844e5b71624b1f7cc139e498
amaddatu <amaddatu@gmail.com>
2018-05-28 17:16:44 -0400

    Add details to the documentation

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,10 @@
 // type "gulp default" into the terminal
 // CTRL + C / CMD + C to cancel
 
+//please note that this configuration has to be restarted periodically due to bugs
+//it doesn't always stop because your code is wrong. (It will simply crash due to race conditions with file locks.)
+//simply restart if it does crash
+
 // Sass configuration
 var gulp = require('gulp');
 var sass = require('gulp-sass');
